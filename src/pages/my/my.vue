ab847390b49b9d0b4850a9b6c2cbbef7b77251e6
DLLZ <792866833@qq.com>
2020-12-23 15:37:10 +0800

    登录页内容完成

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,6 +1,14 @@
 <template>
   <view class='my'>
-
+    <view class="my-main">
+      <view class="text">姓名</view>
+      <view class="text">工号</view>
+      <view class="text">所属工厂</view>
+      <view class="text">当前版本号</view>
+      <view class="text">服务器环境</view>
+      <view class="text">数据库</view>
+      <view class="text">版本日志信息</view>
+    </view>
   </view>
 </template>
 
@@ -38,5 +46,21 @@
 </script>
 
 <style lang='scss' scoped>
+  .my {
+    background: $tbu-content-bg;
+    padding: 20px 0;
+    min-height: $tbu-father-view-min-height;
 
+    .my-main {
+      margin: 0 auto;
+      padding: 5px;
+      width: 90%;
+      border-radius: 10px;
+      background: #ffffff;
+
+      .text {
+        @include tbu-view-text
+      }
+    }
+  }
 </style>
